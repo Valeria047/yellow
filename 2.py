f8f2021038acd2c81a02eadd19b7722cd8e8a0f7
valeri <kotelkina_v04@mail.ru>
2022-02-28 16:41:37 +0300

    commit

---
+++ 2.py
@@ -0,0 +1,35 @@
+import pygame
+import random
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui.ui', self)
+        self.but.clicked.connect(self.cl)
+
+    def cl(self):
+        self.close()
+        return
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    if app.exec_():
+        f = 1
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    screen.fill((0, 0, 0))
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = True
+        pygame.draw.circle(screen, (255, 255, 0), (random.randint(1, 500), random.randint(1, 500)), random.randint(1, 100), 2)
+        pygame.display.flip()
+        clock.tick(3)
